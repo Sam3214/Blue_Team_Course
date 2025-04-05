e67de60c730f9cb1ba2a766f650bd954cac0e3cd
Samarth <59115736+Sam3214@users.noreply.github.com>
2025-04-05 09:33:08 +0530

    Add files via upload

---
+++ Samples-main/Samples/Samples_python/sample.py
@@ -0,0 +1,41 @@
+
+#print
+print("Stanley")
+
+
+#variables
+x=10 #number
+
+Stanley="I am a hardworking person" #String
+
+
+print(Stanley)
+print(x)
+
+#Data type
+x1="sam" #String
+x2=40 #int
+x3=4.6 #float
+
+
+#operators
+# +-/*^%
+s=2
+r=3
+t=s**r
+print(t)
+
+
+#list
+countries=["India","Canada","USA"]
+print(countries[2])
+
+price=[43,56,"sam",9.4]
+
+#if
+age=10
+
+if age>17:
+    print("Able to vote")
+else:
+    print("Not allowed")
